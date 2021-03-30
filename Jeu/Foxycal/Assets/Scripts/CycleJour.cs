ca0c21a24e31a8722b9b7db9ac25c381b819b9fb
JonathanRivest <jonathan_romero@hotmail.fr>
2021-03-30 11:39:36 -0400

    Slider + continuation AI des ennemis

--- Jeu/Foxycal/Assets/Scripts/CycleJour.cs
+++ Jeu/Foxycal/Assets/Scripts/CycleJour.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CycleJour : MonoBehaviour
 {
     public Transform lumiere;
     public int vitesseRotation;
     public static bool tempsJournee;
+    public Slider slider;
 
     void Update()
     {
+        vitesseRotation = 17;
         lumiere.Rotate(vitesseRotation * Time.deltaTime, 0, 0);
         if(lumiere.eulerAngles.x > 179)
         {
-            vitesseRotation = 25;
             tempsJournee = true;
+            slider.value -= 0.002f;
+
         }
         else
         {
-            vitesseRotation = 50;
             tempsJournee = false;
+            slider.value += 0.002f;
         }
     }
+
 }
