5613d3651c5b88cf1c0f33f9645faa5c40d1be20
Gui-Vez <vezina.guillaume@outlook.com>
2021-03-30 08:25:15 -0400

    Avancement Inventaire

---
+++ Jeu/Foxycal/Assets/Scripts/GestionInventaire.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GestionInventaire : MonoBehaviour
+{
+    public bool[] rempli;
+    public GameObject[] boites;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Jeu/Foxycal/Assets/Scripts/RamasserObjet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RamasserObjet : MonoBehaviour
+{
+    private GestionInventaire inventaire;
+
+    void Start()
+    {
+        // Raccourci au script GestionInventaire
+        inventaire = GameObject.FindGameObjectWithTag("Player").GetComponent<GestionInventaire>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Si le trigger est le joueur,
+        if (other.CompareTag("Player"))
+        {
+            // En comptant chaque boite de l'inventaire,
+            for (int i = 0; i < inventaire.boites.Length; i++)
+            {
+                // Si cette boite n'est pas remplie,
+                if (inventaire.rempli[i] == false)
+                {
+                    // L'objet est rentré dans l'inventaire
+                    inventaire.rempli[i] = true;
+
+                    // Arrêter la boucle
+                    break;
+                }
+            }
+        }
+    }
+}
