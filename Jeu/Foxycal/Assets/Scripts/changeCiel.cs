5681e45c3a0196fe5a5ce94fc433bf982e3f111d
Tristan Lapointe <tristan.lapointe02@gmail.com>
2021-04-23 23:34:23 -0400

    Reglages de lumieres.

--- Jeu/Foxycal/Assets/Scripts/changeCiel.cs
+++ Jeu/Foxycal/Assets/Scripts/changeCiel.cs
@@ -9,6 +9,9 @@ public class changeCiel : MonoBehaviour
 
     public Material cielJour;
     public Material cielNuit;
+    public GameObject lumiereJour;
+    public GameObject lumiereNuit;
+    public GameObject[] torches;
 
     // Update is called once per frame
     void Update()
@@ -18,7 +21,16 @@ public class changeCiel : MonoBehaviour
         {
             // Mettre le ciel de nuit
             RenderSettings.skybox = cielNuit;
+            RenderSettings.ambientIntensity = 0.25f;
+            lumiereJour.GetComponent<Light>().color = new Color(207f/255f, 159f/255f, 245f/255f);
+            lumiereJour.GetComponent<Light>().intensity = 0f;
+            lumiereNuit.SetActive(true);
+            foreach (GameObject torche in torches)
+            {
+                torche.SetActive(true);
+            }
             
+
         }
 
         // Sinon,
@@ -26,6 +38,14 @@ public class changeCiel : MonoBehaviour
         {
             // Mettre le ciel de jour
             RenderSettings.skybox = cielJour;
+            RenderSettings.ambientIntensity = 0.5f;
+            lumiereJour.GetComponent<Light>().color = new Color(250f/255f, 255f/255f, 177f/255f);
+            lumiereNuit.SetActive(false);
+            lumiereJour.GetComponent<Light>().intensity = 1f;
+            foreach (GameObject torche in torches)
+            {
+                torche.SetActive(false);
+            }
         }
     }
 }
