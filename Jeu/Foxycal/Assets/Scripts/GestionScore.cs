5a550dc50721b4f7b5b81399f424f7d2f08d96ec
Tristan Lapointe <tristan.lapointe02@gmail.com>
2021-04-27 09:48:09 -0400

    Score

--- Jeu/Foxycal/Assets/Scripts/GestionScore.cs
+++ Jeu/Foxycal/Assets/Scripts/GestionScore.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GestionScore : MonoBehaviour
 {
     /// Auteur : Guillaume Vézina
     /// Description : Gère le pointage obtenu lors du jeu
 
-    public GameObject GestionnaireScore;
+    public GameObject GestionnaireScoreJeu;
     public static int score;
+    public static bool dontDestroyFait;
 
+    private void Start()
+    {
+        if (dontDestroyFait == false)
+        {
+            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(GestionnaireScoreJeu);
+            dontDestroyFait = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+            //Destroy(GestionnaireScoreJeu);
+        }
+    }
     void Update()
     {
-        GestionnaireScore.GetComponent<Text>().text = score.ToString();
+        if (SceneManager.GetActiveScene().name != "sceneFin")
+        {
+            GestionnaireScoreJeu.GetComponent<Text>().text = score.ToString();
+        }
+
     }
 }
